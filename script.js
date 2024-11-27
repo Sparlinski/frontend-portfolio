9742530453c615f3457e2c4b4d0ea2d9e99d8716
Sparlinski <sparlinski@gmail.com>
2024-11-27 16:12:05 -0600

    Fixed console error and refactored script for menu function.

--- script.js
+++ script.js
@@ -3,83 +3,64 @@
 const menuBtn = document.querySelector('.menu_btn');
 const offScreenMenu = document.querySelector('.off-screen_menu');
 
-menuBtn.addEventListener('click', () => {
-    const menuWidth = parseInt(getComputedStyle(offScreenMenu).width, 10);
+// Declare menuWidth globally
+let menuWidth = parseInt(getComputedStyle(offScreenMenu).width, 10);
 
-    if (offScreenMenu.style.left === `-${menuWidth}px` || offScreenMenu.style.left === '') {
-        offScreenMenu.style.left = '0';
-    } else {
-        offScreenMenu.style.left = `-${menuWidth}px`;
+// Update menuWidth whenever the window is resized
+window.addEventListener('resize', () => {
+    menuWidth = parseInt(getComputedStyle(offScreenMenu).width, 10);
+});
+
+menuBtn.addEventListener('click', () => {
+    const isLargeScreen = window.matchMedia('(min-width: 1450px)').matches;
+
+    // If viewport width is 1450px or greater, prevent off-screen menu from moving
+    if (!isLargeScreen) {
+        if (offScreenMenu.style.left === `-${menuWidth}px` || offScreenMenu.style.left === '') {
+            offScreenMenu.style.left = '0';
+        } else {
+            offScreenMenu.style.left = `-${menuWidth}px`;
+        }
+        menuBtn.classList.toggle('active');
     }
-    menuBtn.classList.toggle('active');
 });
 
 /* SCROLL INTO VIEW FUNCTION */
 
-const home = document.querySelector('a[href="#home"]');
-const about = document.querySelector('a[href="#about"]');
-const resume = document.querySelector('a[href="#resume"]');
-const skills = document.querySelector('a[href="#skills"]');
-const projects = document.querySelector('a[href="#projects"]');
-const contactInfo = document.querySelector('a[href="#contact_info"]');
+const menuLinks = [
+    { selector: 'a[href="#home"]', target: 'home' },
+    { selector: 'a[href="#about"]', target: 'about' },
+    { selector: 'a[href="#resume"]', target: 'resume' },
+    { selector: 'a[href="#skills"]', target: 'skills' },
+    { selector: 'a[href="#projects"]', target: 'projects' },
+    { selector: 'a[href="#contact_info"]', target: 'contact_info' }
+];
 
-home.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
-    offScreenMenu.style.left = `-${menuWidth}px`;
-    menuBtn.classList.toggle('active');
-});
+menuLinks.forEach(link => {
+    const element = document.querySelector(link.selector);
 
-about.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
-    offScreenMenu.style.left = `-${menuWidth}px`;
-    menuBtn.classList.toggle('active');
-});
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.getElementById(link.target).scrollIntoView({ behavior: 'smooth' });
 
-resume.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById('resume').scrollIntoView({ behavior: 'smooth' });
-    offScreenMenu.style.left = `-${menuWidth}px`;
-    menuBtn.classList.toggle('active');
-});
+        const isLargeScreen = window.matchMedia('(min-width: 1450px)').matches;
 
-skills.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById('skills').scrollIntoView({ behavior: 'smooth' });
-    offScreenMenu.style.left = `-${menuWidth}px`;
-    menuBtn.classList.toggle('active');
-});
-
-projects.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
-    offScreenMenu.style.left = `-${menuWidth}px`;
-    menuBtn.classList.toggle('active');
-});
-
-contactInfo.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById('contact_info').scrollIntoView({ behavior: 'smooth' });
-    offScreenMenu.style.left = `-${menuWidth}px`;
-    menuBtn.classList.toggle('active');
+        // Prevent the menu from closing on large screens
+        if (!isLargeScreen) {
+            offScreenMenu.style.left = `-${menuWidth}px`;
+            menuBtn.classList.toggle('active');
+        }
+    });
 });
 
 /* AUTO TYPING FUNCTION */
 
 /* SPLASH PAGE A TYPE FUNCTION */
 
-const typingText = document.querySelector('.typing_words')
-const words = [
-    "...DESIGNER", 
-    "...CREATOR", 
-    "...PROBLEM-SOLVER"
-];
-
-let wordIndex = 0;
-let charIndex = 0;
-let deleteChar = false;
-typeAni = document.querySelector('.type_animation');
+const typingText = document.querySelector('.typing_words');
+const words = ["...DESIGNER", "...CREATOR", "...PROBLEM-SOLVER"];
+let wordIndex = 0, charIndex = 0, deleteChar = false;
+const typeAni = document.querySelector('.type_animation');
 
 const autoType = () => {
     const currentWord = words[wordIndex];
@@ -97,27 +78,17 @@ const autoType = () => {
         wordIndex = !deleteChar ? (wordIndex + 1) % words.length : wordIndex;
         setTimeout(autoType, 2000);
     }
-    if (charIndex === 0 || charIndex > currentWord.length - 1) {
-        typeAni.style.animation = "blink 0.7s infinite";
-    } else {
-        typeAni.style.animation = "none"};
+    typeAni.style.animation = (charIndex === 0 || charIndex > currentWord.length - 1) ? "blink 0.7s infinite" : "none";
 };
 
 autoType();
 
 /* SPLASH PAGE B TYPE FUNCTION */
 
-const typingText_b = document.querySelector('.typing_words_b')
-const words_b = [
-    "...DESIGNER", 
-    "...CREATOR", 
-    "...PROBLEM-SOLVER"
-];
-
-let wordIndex_b = 0;
-let charIndex_b = 0;
-let deleteChar_b = false;
-typeAni_b = document.querySelector('.type_animation_b');
+const typingText_b = document.querySelector('.typing_words_b');
+const words_b = ["...DESIGNER", "...CREATOR", "...PROBLEM-SOLVER"];
+let wordIndex_b = 0, charIndex_b = 0, deleteChar_b = false;
+const typeAni_b = document.querySelector('.type_animation_b');
 
 const autoType_b = () => {
     const currentWord = words_b[wordIndex_b];
@@ -135,10 +106,7 @@ const autoType_b = () => {
         wordIndex_b = !deleteChar_b ? (wordIndex_b + 1) % words_b.length : wordIndex_b;
         setTimeout(autoType_b, 2000);
     }
-    if (charIndex_b === 0 || charIndex_b > currentWord.length - 1) {
-        typeAni_b.style.animation = "blink 0.7s infinite";
-    } else {
-        typeAni_b.style.animation = "none"};
+    typeAni_b.style.animation = (charIndex_b === 0 || charIndex_b > currentWord.length - 1) ? "blink 0.7s infinite" : "none";
 };
 
 autoType_b();
@@ -149,5 +117,13 @@ const backToTop = document.querySelector('.back_to_top');
 
 backToTop.onclick = function(e) {
     e.preventDefault();
-    window.scrollTo({top: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+
+
+
+
+
+
+
